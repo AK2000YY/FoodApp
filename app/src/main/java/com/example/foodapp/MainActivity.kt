5b27497841fbce22d2d4154d565fe71da7558297
Abdulkarim <abdyabkor@gmail.com>
2024-10-19 03:05:58 +0300

    add permission and ai model.

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.foodapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.foodapp.navigation.NavSetup
@@ -16,6 +19,13 @@ class MainActivity : ComponentActivity() {
     private lateinit var navHostController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.CAMERA), 0
+            )
+        }
+
         enableEdgeToEdge()
         setContent {
             FoodAppTheme {
@@ -24,4 +34,9 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this, android.Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
 }
