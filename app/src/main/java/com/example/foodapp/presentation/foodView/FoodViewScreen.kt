9fb5dcf19da28d68f0cf11c87dddbe3049f7e37c
Abdulkarim <abdyabkor@gmail.com>
2024-12-11 15:20:28 +0300

    modify FoodViewScreen.

--- app/src/main/java/com/example/foodapp/presentation/foodView/FoodViewScreen.kt
+++ app/src/main/java/com/example/foodapp/presentation/foodView/FoodViewScreen.kt
@@ -1,30 +1,48 @@
 package com.example.foodapp.presentation.foodView
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.foodapp.core.classes.Utils
 import com.example.foodapp.core.component.CustomProgressBar
+import com.example.foodapp.domain.model.NutritionalValue
 import com.example.foodapp.domain.model.Response
 import com.example.foodapp.navigation.sharedData.SharedViewModel
 import com.example.foodapp.presentation.foodView.component.ImagePart
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun FoodViewScreen(
     modifier: Modifier = Modifier,
-    viewModel: SharedViewModel = hiltViewModel()
+    viewModel: SharedViewModel = hiltViewModel(),
+    foodViewViewModel: FoodViewViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
     val context = LocalContext.current
@@ -41,12 +59,35 @@ fun FoodViewScreen(
                 viewModel.saveFood()
             }
         )
-        Text(
+        Box(
             modifier = Modifier
-                .padding(top = 20.dp, start = 8.dp),
-            text = "Name : ${state.name}",
-            style = MaterialTheme.typography.bodyLarge
-        )
+                .padding(8.dp)
+                .fillMaxWidth()
+                .border(
+                    width = 2.dp,
+                    shape = RoundedCornerShape(15.dp),
+                    color = Color.Black
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            when(val response = foodViewViewModel.response) {
+                is Response.Success ->
+                    if(!response.data) CustomProgressBar()
+                    else
+                        NutritionalValueDetail(
+                            nutritionalValue = foodViewViewModel.nutritionalValue,
+//                            modifier = Modifier.fillMaxSize()
+                        )
+                is Response.Loading ->
+                    CustomProgressBar()
+                is Response.Failure ->
+                    Text("something is error")
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        foodViewViewModel.findItemByName(state.name!!)
     }
 
     when(val response = viewModel.response) {
@@ -61,5 +102,77 @@ fun FoodViewScreen(
             LaunchedEffect(response.e) {
                 Utils.showMessage(context, response.e.message)
             }
+    }
+}
+
+
+@Composable
+fun NutritionalValueDetail(
+    nutritionalValue: NutritionalValue,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        NutritionalValueOne(firstText = "Name" , secondText = nutritionalValue.name!!)
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+        NutritionalValueOne(firstText = "Energy" , secondText = "${nutritionalValue.energy!!} kal")
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+        NutritionalValueOne(firstText = "Fats" , secondText = "${nutritionalValue.fats!!} g")
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+        NutritionalValueOne(firstText = "Saturated Fatty Acids" , secondText = "${nutritionalValue.saturatedFattyAcids!!} g")
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+        NutritionalValueOne(firstText = "Sugars" , secondText = "${nutritionalValue.sugars!!} g")
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(Color.Black))
+        NutritionalValueOne(firstText = "Proteins" , secondText = "${nutritionalValue.proteins!!} g")
+    }
+}
+
+@Composable
+fun NutritionalValueOne(
+    firstText: String,
+    secondText: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = firstText,
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .weight(1f)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(2.dp)
+                .background(Color.Black)
+        )
+        Text(
+            text = secondText,
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .weight(1f)
+        )
     }
 }
